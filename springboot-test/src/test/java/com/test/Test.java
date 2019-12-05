01f38dde1bdc5be26588eccecf4b3469f334b0ca
fangjun <fangjun@yxqiche.com>
2019-12-05 09:40:55 +0800

    测试项目

---
+++ springboot-test/src/test/java/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+/**
+ * 测试
+ * <p>
+ * Version		1.0.0
+ *
+ * @author fangjun
+ * <p>
+ * Date	      2019/12/5 9:34
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
